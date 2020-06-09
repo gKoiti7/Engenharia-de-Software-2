57648bda07c8169716d4b06ab82baa9ad3a3be53
Gustavo Koiti Kuwabata <61661132+gKoiti7@users.noreply.github.com>
2020-06-09 10:10:08 -0300

    Atualizações
    Atualizações em geral

--- HTML_gK/01valor.php
+++ HTML_gK/01valor.php
@@ -6,11 +6,6 @@
 		<title>Arquivo PHP+HTML</title>
 		<link rel="stylesheet" type="text/css" href="style.css"/>
 		<link rel="shortcut icon" href="logo.png" sizes="32x32" type="image/png">
-		<?php
-			$txt=isset($_GET["t"])?$_GET["t"]:"Texto Genérico";
-			$tam=isset($_GET["tam"])?$_GET["tam"]:"12pt";
-			$cor=isset($_GET["cor"])?$_GET["cor"]:"black";
-		?>
 	</head>
 	
 	<body>
@@ -25,73 +20,50 @@
 					<a href="index.html">Home</a>
 					<a href="criadores.html">Sobre os desenvolvedores</a>
 					<a href="#">Serviços</a>
-					<a href="fale_conosco.html">Fale conosco</a>
+					<a href="fale_conosco.html">Feedback</a>
 				</div>
 			</div>
 		</header>
 		
-		<div id="principal">
-		<h1>Arquivo PHP+HTML</h1>
-		<?php
-			$d=isset($_GET["ds"])?$_GET["ds"]:0;
-			switch($d){
-				case 2:
-				case 3:
-				case 4:
-				case 5:
-				case 6:
-					echo "Levanta e vai estudar!";
-					break;
-				case 7:
-				case 8:
-					echo "Descansar!";
-					break;
-				default:
-					echo "Dia da semana inválido";
-			}
-				
-			/*
-			$n=isset($_GET["num"])?$_GET["num"]:0;
-			$o=isset($_GET["oper"])?$_GET["oper"]:1;
-			switch($o){
-				case 1:
-					$r=$n*2;
-					break;
-				case 2:
-					$r=$n^3;
-					break;
-				case 3:
-					$r=sqrt($n);
-					break;
-			}
-			echo "O resultado da operação solicitada foi <span class='foco'>$r</span>"; 
-			
-			$a=isset($_GET["ano"])?$_GET["ano"]:1900;
-			$i=date("Y")-$a;
-			echo "Você nasceu em $a e terá $i anos";
-			if($i<16){
-				$tipoVoto="Não vota";
-			}
-			elseif(($i>=16 && $i<18)||($i>65)){
-					$tipoVoto="Voto opcional";
+		<div id="fale_conosco">
+			<h1>Arquivo PHP+HTML</h1>
+			<?php
+				$nome=isset($_GET["nome"])?$_GET["nome"]:"[nome não inserido]";
+				echo "Nome do usuário: $nome";
+				echo"</br>";
+				$ano=isset($_GET["nascimento"])?$_GET["nascimento"]:"[idade não inserida]";
+				$idade=date("Y")-$ano;
+				echo "Idade do usuário: $idade";
+				echo "</br>";
+				$email=isset($_GET["e-mail"])?$_GET["e-mail"]:"[e-mail não inserido]";
+				echo "E-mail do usuário: $email";
+				echo "</br>";
+				$opcao=isset($_GET["aval"])?$_GET["aval"]:0;
+				switch($opcao){
+					case 1:
+						echo "Avaliação do usuário: Ótimo";
+						break;
+					case 2:
+						echo "Avaliação do usuário: Bom";
+						break;
+					case 3:
+						echo "Avaliação do usuário: Mediano";
+						break;
+					case 4:
+						echo "Avaliação do usuário: Ruim";
+						break;
+					case 5:
+						echo "Avaliação do usuário: Péssimo";
+						break;
+					default:
+						echo "Avaliação do usuário: [Não inserido]";
+						break;
 				}
-				else{
-					$tipoVoto="Voto obrigatório";
-				}
-			echo "Para essa idade, $tipoVoto";
-			
-			echo "<span class='texto'>$txt</span>";
-			$nome=isset($_GET["nome"])?$_GET["nome"]:"[não informado]";
-			$ano=isset($_GET["ano"])?$_GET["ano"]:0;
-			$sexo=isset($_GET["sexo"])?$_GET["sexo"]:"[sem sexo]";
-			$idade=date("Y")-$ano;
-			echo "<br/>$nome é $sexo e tem $idade anos.";
-			*/
-		?>
-		<!--<a href="index.html">Voltar</a>-->
-		<br/><a href="javascript:history.go(-1)">Voltar</a>
+			?>
+			<div id="menu">
+				<a href="javascript:history.go(-1)">Voltar</a>
+			</div>
 		</div>
-		
 		<!--Rodapé-->
 		<footer>
 			Desenvolvido por:
---
+++ HTML_gK/02_tabuada.php
@@ -0,0 +1,68 @@
+<DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css"/>
+	<meta charset="UTF=8"/>
+	<title>Tabuada</title>
+</head>
+<body>
+<div>
+	<?php
+		
+		/*
+		$t="Aqui temos um texto gigante e vai mostrar o funcionamento da função wordwrap";
+		$r=wordwrap($t,5,"</br>\n", false);
+		echo $r;
+		
+		$v[0]=1;
+		$v[1]=2;
+		$v[2]=5;
+		print_r($v);
+		echo "</br>";
+		$v2=array(3,7,6,2,1,9);
+		var_export($v2);
+		
+		$p="leite";
+		$pr=4.5;
+		printf("O %s custa R$ %.2f",$p,$pr);
+		
+		function teste(&$x){
+			$x+=2;
+			echo "O valor de X é $x</p>";
+		}
+		$a=3;
+		teste($a);
+		echo "O valor de A é $a</p>"
+		
+		function soma(){
+			$p=func_get_args();
+			$t=func_num_args();
+			$s=0;
+			for($i=0;$i<$t;$i++){
+				$s+=$p[$i];
+			}
+			return $s;
+		}
+		$r=soma(3,4,5,3,5);
+		echo "A soma dos valores é $r";
+		
+		function soma($a,$b){
+			return $a+$b;
+			}
+		$x=3;
+		$y=2;
+		$r=soma($x,$y);
+		echo "A soma entre $x e $y é igual a $r";
+		
+		$n=isset($_GET["num"])?$_GET["num"]:1;
+		for($c=1;$c<=10;$c++){
+			$r=$n*$c;
+			echo "$n x $c = $r";
+			echo "</br>";
+		}
+		*/
+	?>
+	</br><a href="javascript:history.go(-1)">Voltar</a>
+</div>
+</body>
+</html>
